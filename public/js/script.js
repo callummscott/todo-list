b790ca70ddb72488770acc3dfdbabe9314a8dd37
Callum <callummscott@yahoo.co.uk>
2026-05-11 13:17:53 +0100

    Tidy CSS and names, finish `Esc`/`Enter` behaviour - Remove outdated/unused CSS - Finalise `Esc` to undo and `Enter` to save functionality - Switch to 'input blur triggers submission' functionality - Clarify element naming convention - Move `this.form.submit()` HTML to `script.js`

--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,54 @@
-/* Return the user back to their previous scroll height after redirect. */
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var scrollpos = sessionStorage.getItem('scrollpos');
+/*
+Return the user back to their previous scroll height after redirect.
+ > It gets disorienting and frustrating otherwise.
+*/
+document.addEventListener("DOMContentLoaded", (event) => { 
+  const scrollpos = sessionStorage.getItem('scrollpos');
   if (scrollpos) window.scrollTo(0, scrollpos);
-  sessionStorage.removeItem('scrollpos')
+  sessionStorage.removeItem('scrollpos');
 });
 
-window.onbeforeunload = function(e) {
-    sessionStorage.setItem('scrollpos', window.scrollY);
-};
+window.onbeforeunload = (e) => sessionStorage.setItem('scrollpos', window.scrollY);
 
 
-/* Adds `Esc` to cancel and `Enter` to submit functionality. */
+/*
+The functions below help prevent new anonymous functions being created
+for every single `<textarea>` element during the `textareas.forEach(...)`
+iteration — that's why they're a bit janky.
+*/
+
 document.querySelectorAll('textarea').forEach(element => {
   let originalText = "";
-  element.addEventListener('keydown', (e) => {
+  const textHasChanged = () => element.value !== originalText;
+  
+  /* Adds `Esc` to cancel and `Enter` to submit functionality. */
+  element.addEventListener('keydown', e => {
     switch (e.key) {
       // Prevent newlines and submit edited text.
       case "Enter":
         e.preventDefault();
-        element.closest('form').requestSubmit();
+        if (textHasChanged()) {
+          element.closest('form').requestSubmit();
+          break;
+        }
+        document.activeElement.blur();
         break;
       // Cancel any changes to the text content
       case "Escape":
-        document.activeElement.blur(); 
-        // this triggers the `blur` event listener, resetting the text.
+        element.value = originalText;
+        document.activeElement.blur();
         break;
     }
   });
-  element.addEventListener('focus', () => {
-    originalText = element.value;
-  });
+
+  element.addEventListener('focus', () => originalText = element.value);
   element.addEventListener('blur', () => {
-    element.value = originalText;
-  })
+    if (textHasChanged()) element.closest('form').requestSubmit();
+  });
 });
 
-/* Delete item checkbox form submission */
-document.querySelectorAll('.item-checkbox').forEach(checkbox => {
-  checkbox.addEventListener('change', () => {
-    checkbox.closest('form').requestSubmit();
-  });
+/* Delete-item checkbox form submission */
+//  > Replaces `this.form.submit()` in the HTML.
+document.querySelectorAll('.del-checkbox').forEach(checkbox => {
+  checkbox.addEventListener('change', () => checkbox.closest('form').requestSubmit());
 });
